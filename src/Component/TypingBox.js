095aaf3743461e0c15858417800588a1c9b2d64e
khushalpatil093 <khushalpatil093@gmail.com>
2023-10-14 20:35:43 +0530

    under progress

---
+++ src/Component/TypingBox.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './Styles/styles.css'
+
+const TypingBox = () => {
+    return (
+        <>
+            <textarea rows="10" cols="70"></textarea>
+        </>
+    )
+}
+
+export default TypingBox
