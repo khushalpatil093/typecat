c4e15c0351a03ccd3a34eee5bf2acb2ba4a78d67
khushalpatil093 <khushalpatil093@gmail.com>
2024-02-18 13:37:18 +0530

    done

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -7,6 +7,9 @@ const Header = () => {
         <div className="header">
             <div className="logo">
                 <KeyboardIcon/>
+                <div>
+                    <h2>typecat</h2>
+                </div>
             </div>
             <div className="user-icon">
                 <AccountCircle/>
